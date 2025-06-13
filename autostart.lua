279e217074a4a611827729282e5343ea31c5d8cf
Rustam Uzairov <svrvt.zrv@gmail.com>
2025-06-13 21:04:26 +0300

    mini change

--- autostart.lua
+++ autostart.lua
@@ -11,14 +11,11 @@ local function run_once(cmd)
 	awful.spawn.with_shell(string.format("pgrep -u $USER -x %s > /dev/null || (%s)", findme, cmd), false)
 end
 
-ImaGes = "still"
-if hostname == "pcRU" or "vaio" then
-	ImaGes = "unladyes"
-end
+local pictures_dir = "~/mount/yandex/wallpapers"
+local pictures_set = "unladyes" -- still, down, unladyes
 
-run_once("feh --bg-fill --randomize ~/mount/yandex/wallpapers/" .. ImaGes)
+run_once("feh --bg-fill --randomize" .. pictures_dir .. "/" .. pictures_set)
 
 -- run_once('xmodmap -e "pointer = 3 2 1 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 4 5')
--- run_once("~/.local/bin/greenclip daemon")
 
 -- return autostart
